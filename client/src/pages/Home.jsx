77dd3a1a5eaa80b7bfe02239f62cf0007d5a0dc4
Brainspeck <erikmaloschuessler@gmail.com>
2022-09-04 19:42:20 -0500

    Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import '../styles/home.css';
 export default function Home(){
     return(
-        <div>
-            Home
-        </div>
+            <div className="homeBackground">
+                <h1 className="title">HOME</h1>
+
+            </div>
+    
     )
 }
